011b92196601b1fcc61f65e68a2dfa752070650d
Tomek <tomaszgaj80@gmail.com>
2016-07-11 21:02:37 +0200

    Functional test now check we can input a to-do item

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return HttpResponse('<html><title>To-Do lists</title><h1>To-Do</h1></html>')
